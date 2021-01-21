c0a82c1f6d653f234c09dc2f5862f38eda24129e
Stacy Gaikovaia <stacygaikovaia@gmail.com>
2021-01-21 14:28:48 -0500

    Create PyBulletEnv

---
+++ python_part/PyBulletEnv.py
@@ -0,0 +1,29 @@
+import pybullet_data
+import time
+import pybullet as p
+
+class PyBulletEnv:
+    #camera = Camera()
+    num = 2
+
+    def __init__(self):
+        print("Hello")
+
+    def setup(self):
+        p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        #p.resetSimulation()
+        # Make the floor...
+        p.loadURDF("plane100.urdf", useMaximalCoordinates=True)
+
+        #disable rendering during creation.
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        #disable tinyrenderer, software (CPU) renderer, we don't use it here
+        p.configureDebugVisualizer(p.COV_ENABLE_TINY_RENDERER, 0)
+
+    def run(self):
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
+        p.setRealTimeSimulation(1)
+        while(1):
+            time.sleep(1./240.)
---
+++ python_part/rock.py
@@ -0,0 +1,38 @@
+import pybullet as p
+import time
+import math
+import pybullet_data
+import PyBulletEnv
+
+def trapezoid():
+    base = 0.000624
+    height = 0.172867
+    # A shape wit 8 faces
+    vertices = [
+            [-0.246350, -0.246483, -base], [-0.151407, -0.176325, height],
+            [-0.246350, 0.249205, -base], [-0.151407, 0.129477, height],
+            [0.249338, -0.246483, -base], [0.154395, -0.176325, height],
+            [0.249338, 0.249205, -base], [0.154395, 0.129477, height]
+            ]
+    indices = [
+        0, 3, 2, 3, 6, 2, 7, 4, 6, 5, 0, 4, 6, 0, 2, 3, 5, 7, 0, 1, 3, 3, 7, 6, 7, 5, 4, 5, 1, 0, 6, 4,
+        0, 3, 1, 5
+    ]
+        
+    # create convex mesh from obj!
+    stoneID = p.createCollisionShape(p.GEOM_MESH, vertices=vertices
+                                    #indices=indices
+                                    )
+
+    # initialize and run object :) 
+    p.createMultiBody(baseMass = 0, 
+                    baseCollisionShapeIndex=stoneID, 
+                    basePosition=[0, 0])
+
+
+
+if __name__ == "__main__":
+    env = PyBulletEnv.PyBulletEnv()
+    env.setup()
+    trapezoid()
+    env.run()
