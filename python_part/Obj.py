a8baa55f20da7543e44d1b75b00d165046bd99b2
viatsk <stacygaikovaia@gmail.com>
2021-03-22 23:57:28 -0400

    Fix naming convention diff between Obj and minimizer

--- python_part/Obj.py
+++ python_part/Obj.py
@@ -1,11 +1,14 @@
 import os
 import pybullet as p
+import subprocess
+
+MINIMIZER = 'minimizeWithBlendr.py'
 
 class Obj:
-
+    
     def __init__(self, fn, scale):
         self.filename = os.getcwd()+fn
-        self.smaller_filename = ''
+        self.smaller_filename = None
         self.v = []
         self.meshScale = [scale, scale, scale]
         self.obj = None
@@ -13,14 +16,19 @@ class Obj:
         return    
 
 
-    def smaller(self):
-        # TODO: Add method that calls the mesh decimator!
-        
+    def smaller(self, ratio):
+        name, suff = self.filename.split(".")
+        self.smaller_filename = name + "_" + str(ratio) + "." + suff
+        subprocess.run(["blender", "-b", "-P", MINIMIZER, str(ratio), self.filename])
+        # Once process has completed, we can grab the new dimensions??? 
+
         return None
     
 
     def createObject(self, position):
-        name, suff = self.filename.split('.')
+        #TODO: Add information about the number of vertices!
+        
+        _, suff = self.filename.split('.')
         uniqueID = -1
         if suff == "obj":
             # creating an obj
@@ -65,8 +73,14 @@ class Obj:
         
         
     def change_position(self,roll, pitch, yaw):
+        if self.obj == None:
+            print("Object not initialized!")
+            return -1
+
+        # Otherwise, change it's position :) 
         pos, _ = p.getBasePositionAndOrientation(self.obj)
         orientation = p.getQuaternionFromEuler([roll, pitch, yaw])
         p.resetBasePositionAndOrientation(self.obj, pos, orientation)
+
 
 
--- python_part/minimizeWithBlendr.py
+++ python_part/minimizeWithBlendr.py
@@ -1,29 +1,27 @@
 import bpy
+import argparse
 
 DECIMATION_RATIO = 0.01
 
 '''
 Class MeshDecimator:
 Calls headless blender in order to decimate relevant Obj instance
-Author: Stacy Gaikovaia 
 
+Due to Blender's very numerous limitations, we can only call this as a subprocess
+Run as `blender -b -P minimizeWithBlendr.py -- <ratio> <obj location>`
+
+Author: Stacy Gaikovaia 
 
 '''
 class MeshDecimator:
     def __init__(self):
-
         return
 
     def delete_crap(self):
         bpy.ops.object.select_all(action="DESELECT")
         # Blender has a bunch of pre-done objects
         # stuff = ["Camera", "Cube", "Lamp"]
-        #stuff = ["Cube"]
-        #bpy.data.objects.remove(bpy.data.objects[0])
-        print(list(bpy.data.objects))
-        print(bpy.data.objects['Cube'])
-
-        # FOR OLDER API: 
+        # FOR OLDER API (< 2.8):  
         '''
         for s in stuff: 
             bpy.data.objects[s].select = True
@@ -41,17 +39,19 @@ class MeshDecimator:
         return meshes
 
 
-    def main(self, input_model):
+    def main(self, ratio, input_model):
+        #print(ratio)
+        #print(input_model)
         inpa, inpb = input_model.split('.')
-        output_model = inpa + "_SMALLER" + '.' + inpb
+        output_model = inpa + "_" + str(ratio) + '.' + inpb
         self.delete_crap()
         bpy.ops.import_scene.obj(filepath=input_model)
 
         meshes = self.get_meshes()
 
-        print("meshes: ")
-        print(meshes)
-        print(list(bpy.data.objects))
+        #print("meshes: ")
+        #print(meshes)
+        #print(list(bpy.data.objects))
         
 
         for (_, obj) in enumerate(meshes):
@@ -60,20 +60,31 @@ class MeshDecimator:
             #print(bpy.context.active_object)
             bpy.context.view_layer.objects.active = obj
             modifier = obj.modifiers.new('DecimateMod', 'DECIMATE')
-            modifier.ratio = DECIMATION_RATIO
+            modifier.ratio = ratio
             modifier.use_collapse_triangulate = True
             # NEW API uses: 
             bpy.ops.object.modifier_apply(modifier = 'DecimateMod')
             # Old API : 
             # bpy.ops.object.modifier_apply(apply_as='DATA', modifier = 'DecimateMod')
+            # TODO: Save these values by returning them!!
             print("{} has {} verts, {} edges, {} polys after decimation".format(obj.name, len(obj.data.vertices), len(obj.data.edges), len(obj.data.polygons)))
 
         bpy.ops.export_scene.obj(filepath=output_model)
         print("Written to file " + output_model)
+        return 
 
 # main('data/trees9.obj')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Blender Mesh Decimator")
+    _, args = parser.parse_known_args()
+    for_this_script = args[args.index('--')+1 : ]
+    parser.add_argument('ratio', metavar='R', type=float, help='Decimation ratio')
+    parser.add_argument('filename', metavar='F', type=str, help='An .obj file')
+    
+    myArgs = parser.parse_args(for_this_script)
+    #print(myArgs)
 
-md = MeshDecimator()
-md.main('data/deer.obj')
+    md = MeshDecimator()
+    md.main(myArgs.ratio, myArgs.filename)
 
 
