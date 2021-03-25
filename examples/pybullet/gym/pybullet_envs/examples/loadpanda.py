78f95117e70d0723563dba7801d47ed790c80a53
Daniel Chung <daniel@tailscale.com>
2021-03-25 03:29:59 -0400

    Add capability to update on interval, make numbers more descriptive

--- examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
+++ examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
@@ -18,26 +18,34 @@ panda = panda_sim.PandaSim(p,[0,0,0])
 
 frameCounter = 0
 
+start_time = time.time()
+update_interval = 1
+prev_count = 0 
+
 while (1):
-	start_time = time.time()
 	panda.step()
 	p.stepSimulation()
-	#time.sleep(timeStep)
+	time.sleep(timeStep)
 	frameCounter += 1
-	#print(frameCounter)
 	numBodies = p.getNumBodies()
-	print(numBodies)
-	"""p.addUserDebugText(str(frameCounter), [10, 0, 0.1],
-				textColorRGB=[1, 0, 0],
-				textSize=1.5,
-				lifeTime=0.5)
+	# print(numBodies)
 
-	p.addUserDebugText(str(1.0/(time.time() - start_time)), [10, 0, 1],
-				textColorRGB=[1, 0, 0],
-				textSize=1.5,
-				lifeTime=0.5)
-"""
-	p.addUserDebugText(str(numBodies), [10, 0, 1],
-				textColorRGB=[1, 0, 0],
-				textSize=1.5,
- 				lifeTime=0.5)
+	curr_time = time.time()
+	if curr_time - start_time > update_interval:
+		start_time = curr_time
+
+		p.addUserDebugText("framecount: " + str(frameCounter), [10, 1, 0.1],
+					textColorRGB=[1, 0, 0],
+					textSize=1.5,
+					lifeTime=update_interval)
+
+		p.addUserDebugText("fps: " + str((frameCounter - prev_count)/update_interval), [10, 2, 1],
+					textColorRGB=[1, 0, 0],
+					textSize=1.5,
+					lifeTime=update_interval)
+		prev_count = frameCounter
+
+		p.addUserDebugText("numBodies: " + str(numBodies), [10, 0, 0.1],
+					textColorRGB=[1, 0, 0],
+					textSize=1.5,
+					lifeTime=update_interval)
