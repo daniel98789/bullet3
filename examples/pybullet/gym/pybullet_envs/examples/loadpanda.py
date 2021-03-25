8b5450d01524fa5d4989d0bcb7ab729a31aafdd9
Daniel Wang <daniel98789@gmail.com>
2021-03-25 01:53:04 -0400

    fps counter

--- examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
+++ examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
@@ -4,6 +4,7 @@ import math
 import time
 import numpy as np
 from pybullet_envs.examples import panda_sim
+import time
 
 p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP,1)
@@ -18,12 +19,18 @@ panda = panda_sim.PandaSim(p,[0,0,0])
 frameCounter = 0
 
 while (1):
+	start_time = time.time()
 	panda.step()
 	p.stepSimulation()
 	#time.sleep(timeStep)
 	frameCounter += 1
 	print(frameCounter)
 	p.addUserDebugText(str(frameCounter), [10, 0, 0.1],
+				textColorRGB=[1, 0, 0],
+				textSize=1.5,
+				lifeTime=0.1)
+
+	p.addUserDebugText(str(1.0/(time.time() - start_time)), [10, 0, 1],
 				textColorRGB=[1, 0, 0],
 				textSize=1.5,
 				lifeTime=0.1)
