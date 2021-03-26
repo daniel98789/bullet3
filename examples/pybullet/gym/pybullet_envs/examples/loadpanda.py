93fd1861bc27d787e4ae4a5b54d3180d632ff1bc
Daniel Wang <daniel98789@gmail.com>
2021-03-25 22:55:26 -0400

    Clean up profiler ui

--- examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
+++ examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
@@ -10,7 +10,7 @@ p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP,1)
 p.setAdditionalSearchPath(pd.getDataPath())
 
-timeStep=1./60.
+timeStep=1./120.
 p.setTimeStep(timeStep)
 p.setGravity(0,-9.8,0)
  
@@ -19,33 +19,35 @@ panda = panda_sim.PandaSim(p,[0,0,0])
 frameCounter = 0
 
 start_time = time.time()
-update_interval = 1
+update_interval = 0.5
 prev_count = 0 
 
+p.resetDebugVisualizerCamera( cameraDistance=1.4, cameraYaw=40, cameraPitch=-30, cameraTargetPosition=[0,0,0])
+
 while (1):
+	curr_time = time.time()
+
 	panda.step()
 	p.stepSimulation()
 	time.sleep(timeStep)
 	frameCounter += 1
 	numBodies = p.getNumBodies()
-	# print(numBodies)
 
-	curr_time = time.time()
 	if curr_time - start_time > update_interval:
 		start_time = curr_time
 
-		p.addUserDebugText("framecount: " + str(frameCounter), [10, 1, 0.1],
-					textColorRGB=[1, 0, 0],
-					textSize=1.5,
-					lifeTime=update_interval)
+		p.addUserDebugText("Frame Counter: " + str(frameCounter), [10, 1.5, -0.625],
+					textColorRGB=[0, 0, 0],
+					textSize=1.3,
+					lifeTime=update_interval+0.1)
 
-		p.addUserDebugText("fps: " + str((frameCounter - prev_count)/update_interval), [10, 2, 1],
-					textColorRGB=[1, 0, 0],
-					textSize=1.5,
-					lifeTime=update_interval)
+		p.addUserDebugText("FPS: " + str((frameCounter - prev_count)/update_interval), [10, 2, -0.4],
+					textColorRGB=[0, 0, 0],
+					textSize=1.3,
+					lifeTime=update_interval+0.1)
 		prev_count = frameCounter
 
-		p.addUserDebugText("numBodies: " + str(numBodies), [10, 0, 0.1],
-					textColorRGB=[1, 0, 0],
-					textSize=1.5,
-					lifeTime=update_interval)
+		p.addUserDebugText("Body Count: " + str(numBodies), [10, 1, -0.85],
+					textColorRGB=[0, 0, 0],
+					textSize=1.3,
+					lifeTime=update_interval+0.1)
