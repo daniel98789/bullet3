ab376390d4f9deb7ae3435f8bfeec278098fa06d
Daniel Chung <daniel@tailscale.com>
2021-03-26 01:36:55 -0400

    Format floats to 2 decimal points

--- examples/pybullet/gym/pybullet_envs/examples/profiler.py
+++ examples/pybullet/gym/pybullet_envs/examples/profiler.py
@@ -24,7 +24,7 @@ class Profiler:
 
 			
 			if (profile_mask & (1<<0)):
-				self.p.addUserDebugText("FPS: " + str((self.frameCounter - self.prev_count)/self.update_interval), [10, 2, -0.4],
+				self.p.addUserDebugText("FPS: " + "{:.2f}".format((self.frameCounter - self.prev_count)/self.update_interval), [10, 2, -0.4],
 						textColorRGB=[0, 0, 0],
 						textSize=1.3,
 						lifeTime=self.update_interval+0.1)
@@ -44,14 +44,14 @@ class Profiler:
 			if (profile_mask & (1<<2)):
 				position = self.p.getBasePositionAndOrientation(self.pos_obj)
 
-				self.p.addUserDebugText("Position: " + "x: " + str(position[0][0]) + " y: " + str(position[0][1]) + " z: " + str(position[0][2]), 
+				self.p.addUserDebugText("Position: " + "x: " + "{:.2f}".format(position[0][0]) + " y: " + "{:.2f}".format(position[0][1]) + " z: " + "{:.2f}".format(position[0][2]), 
 							[10, 0.5, -1.07],
 							textColorRGB=[0, 0, 0],
 							textSize=1.3,
 							lifeTime=self.update_interval+0.1)
 			
 			if (profile_mask & (1<<3)):
-				self.p.addUserDebugText("Orientation: " "x: " + "{:.2f}".format(position[1][0]) + " y: " + str(position[1][1]) + " z: " + str(position[1][2]), 
+				self.p.addUserDebugText("Orientation: " "x: " + "{:.2f}".format(position[1][0]) + " y: " + "{:.2f}".format(position[1][1]) + " z: " + "{:.2f}".format(position[1][2]), 
 							[10, 0, -1.29],
 							textColorRGB=[0, 0, 0],
 							textSize=1.3,
