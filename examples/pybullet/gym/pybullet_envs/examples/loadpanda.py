0c6fe4d787a3af6f3f4e73acd2ae507bab6bec7c
sglyons <lyons.stephen1@gmail.com>
2021-03-26 00:22:20 -0400

    Extracted Profiler to an external class

--- examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
+++ examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
@@ -4,7 +4,7 @@ import math
 import time
 import numpy as np
 from pybullet_envs.examples import panda_sim
-import time
+import profiler
 
 p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP,1)
@@ -16,10 +16,11 @@ p.setGravity(0,-9.8,0)
  
 panda = panda_sim.PandaSim(p,[0,0,0])
 
-frameCounter = 0
+prof = profiler.Profiler(p,[panda.panda])
+prof.select_object_to_get_pos_ori(panda.panda)
 
 start_time = time.time()
-update_interval = 0.5
+
 prev_count = 0 
 
 p.resetDebugVisualizerCamera( cameraDistance=1.4, cameraYaw=40, cameraPitch=-30, cameraTargetPosition=[0,0,0])
@@ -29,46 +30,10 @@ while (1):
 
 	panda.step()
 	p.stepSimulation()
+	prof.profile(1)
 	time.sleep(timeStep)
-	frameCounter += 1
-	numBodies = p.getNumBodies()
-	position = p.getBasePositionAndOrientation(panda.panda)
-	numJoints = p.getNumJoints(panda.panda)
+
+	
 	#velocity = p.getBaseVelocity(panda.ball1)
 
-	if curr_time - start_time > update_interval:
-		start_time = curr_time
-
-		p.addUserDebugText("Joint Count: " + str(numJoints), 
-					[10, -0.5, -1.5],
-					textColorRGB=[0, 0, 0],
-					textSize=1.3,
-					lifeTime=update_interval+0.1)
-
-		p.addUserDebugText("Position: " + "x: " + str(position[0][0]) + " y: " + str(position[0][1]) + " z: " + str(position[0][2]), 
-					[10, 0.5, -1.07],
-					textColorRGB=[0, 0, 0],
-					textSize=1.3,
-					lifeTime=update_interval+0.1)
-		
-		p.addUserDebugText("Orientation: " "x: " + "{:.2f}".format(position[1][0]) + " y: " + str(position[1][1]) + " z: " + str(position[1][2]), 
-					[10, 0, -1.29],
-					textColorRGB=[0, 0, 0],
-					textSize=1.3,
-					lifeTime=update_interval+0.1)
-
-		p.addUserDebugText("Frame Counter: " + str(frameCounter), [10, 1.5, -0.625],
-					textColorRGB=[0, 0, 0],
-					textSize=1.3,
-					lifeTime=update_interval+0.1)
-
-		p.addUserDebugText("FPS: " + str((frameCounter - prev_count)/update_interval), [10, 2, -0.4],
-					textColorRGB=[0, 0, 0],
-					textSize=1.3,
-					lifeTime=update_interval+0.1)
-		prev_count = frameCounter
-
-		p.addUserDebugText("Body Count: " + str(numBodies), [10, 1, -0.85],
-					textColorRGB=[0, 0, 0],
-					textSize=1.3,
-					lifeTime=update_interval+0.1)
+	
---
+++ examples/pybullet/gym/pybullet_envs/examples/profiler.py
@@ -0,0 +1,67 @@
+import pybullet as p
+import time
+
+
+class Profiler:
+	def __init__(self,bullet_client,bodyUniqueIDList):
+		self.prev_count = 0
+		self.frameCounter = 0
+		self.update_interval = 0.5
+		self.p=bullet_client;
+		self.bodies = bodyUniqueIDList
+		self.start_time = time.time()
+
+	def select_object_to_get_pos_ori(self,bodyUniqueID):
+		self.pos_obj = bodyUniqueID
+
+
+	def profile(self,profile_mask):
+		self.frameCounter += 1
+
+		self.curr_time = time.time()
+		if self.curr_time - self.start_time > self.update_interval:
+			self.start_time = self.curr_time
+
+			numJoints = 0
+			for ID in self.bodies:
+				numJoints += self.p.getNumJoints(ID)
+
+			self.p.addUserDebugText("Joint Count: " + str(numJoints), 
+						[10, -0.5, -1.5],
+						textColorRGB=[0, 0, 0],
+						textSize=1.3,
+						lifeTime=self.update_interval+0.1)
+
+			position = self.p.getBasePositionAndOrientation(self.pos_obj)
+
+			self.p.addUserDebugText("Position: " + "x: " + str(position[0][0]) + " y: " + str(position[0][1]) + " z: " + str(position[0][2]), 
+						[10, 0.5, -1.07],
+						textColorRGB=[0, 0, 0],
+						textSize=1.3,
+						lifeTime=self.update_interval+0.1)
+			
+			self.p.addUserDebugText("Orientation: " "x: " + "{:.2f}".format(position[1][0]) + " y: " + str(position[1][1]) + " z: " + str(position[1][2]), 
+						[10, 0, -1.29],
+						textColorRGB=[0, 0, 0],
+						textSize=1.3,
+						lifeTime=self.update_interval+0.1)
+
+			self.p.addUserDebugText("Frame Counter: " + str(self.frameCounter), [10, 1.5, -0.625],
+						textColorRGB=[0, 0, 0],
+						textSize=1.3,
+						lifeTime=self.update_interval+0.1)
+
+			self.p.addUserDebugText("FPS: " + str((self.frameCounter - self.prev_count)/self.update_interval), [10, 2, -0.4],
+						textColorRGB=[0, 0, 0],
+						textSize=1.3,
+						lifeTime=self.update_interval+0.1)
+			self.prev_count = self.frameCounter
+
+
+			numBodies = self.p.getNumBodies()
+
+			self.p.addUserDebugText("Body Count: " + str(numBodies), [10, 1, -0.85],
+						textColorRGB=[0, 0, 0],
+						textSize=1.3,
+						lifeTime=self.update_interval+0.1)
+
