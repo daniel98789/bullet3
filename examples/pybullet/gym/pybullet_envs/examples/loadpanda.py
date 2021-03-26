631d009cdf0691317a51c74f032acfb7ff2de6b8
Daniel Wang <daniel98789@gmail.com>
2021-03-25 23:37:42 -0400

    added stuff to profiler

--- examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
+++ examples/pybullet/gym/pybullet_envs/examples/loadpanda.py
@@ -32,9 +32,30 @@ while (1):
 	time.sleep(timeStep)
 	frameCounter += 1
 	numBodies = p.getNumBodies()
+	position = p.getBasePositionAndOrientation(panda.panda)
+	numJoints = p.getNumJoints(panda.panda)
+	#velocity = p.getBaseVelocity(panda.ball1)
 
 	if curr_time - start_time > update_interval:
 		start_time = curr_time
+
+		p.addUserDebugText("Joint Count: " + str(numJoints), 
+					[10, -0.5, -1.5],
+					textColorRGB=[0, 0, 0],
+					textSize=1.3,
+					lifeTime=update_interval+0.1)
+
+		p.addUserDebugText("Position: " + "x: " + str(position[0][0]) + " y: " + str(position[0][1]) + " z: " + str(position[0][2]), 
+					[10, 0.5, -1.07],
+					textColorRGB=[0, 0, 0],
+					textSize=1.3,
+					lifeTime=update_interval+0.1)
+		
+		p.addUserDebugText("Orientation: " "x: " + "{:.2f}".format(position[1][0]) + " y: " + str(position[1][1]) + " z: " + str(position[1][2]), 
+					[10, 0, -1.29],
+					textColorRGB=[0, 0, 0],
+					textSize=1.3,
+					lifeTime=update_interval+0.1)
 
 		p.addUserDebugText("Frame Counter: " + str(frameCounter), [10, 1.5, -0.625],
 					textColorRGB=[0, 0, 0],
