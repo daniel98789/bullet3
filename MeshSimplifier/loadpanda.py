8f3425092128c9927b09234ebf48b667231e73d0
Daniel Wang <daniel98789@gmail.com>
2021-03-28 21:13:08 -0400

    fixed camera test

--- MeshSimplifier/loadpanda.py
+++ MeshSimplifier/loadpanda.py
@@ -8,37 +8,42 @@ import profiler
 import debugvisualizer
 
 p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP,1)
+p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP, 1)
 p.setAdditionalSearchPath(pd.getDataPath())
 
-timeStep=1./120.
+timeStep = 1./120.
 p.setTimeStep(timeStep)
-p.setGravity(0,-9.8,0)
- 
-panda = panda_sim.PandaSim(p,[0,0,0])
+p.setGravity(0, -9.8, 0)
+
+panda = panda_sim.PandaSim(p, [0, 0, 0])
 start_time = time.time()
 prev_loc = None
-prof = profiler.Profiler(p,[panda.panda])
+prof = profiler.Profiler(p, [panda.panda])
 prof.select_object_to_get_pos_ori(panda.panda)
 
-p.resetDebugVisualizerCamera( cameraDistance=1.4, cameraYaw=40, cameraPitch=-30, cameraTargetPosition=[0,0,0])
+p.resetDebugVisualizerCamera(
+    cameraDistance=1.4, cameraYaw=40, cameraPitch=-30, cameraTargetPosition=[2, 0, 0])
 x = 1
 y = 0
 z = 0
 while (1):
-	curr_time = time.time()
-	# if curr_time - start_time > 2:
-	# 	found, loc = debugvisualizer.object_is_in_frame(p, panda.panda, prev_loc)
-	# 	if found:
-	# 		prev_loc = loc
-	# 	start_time = curr_time
-	# 	x = x + 1
-	# 	cam_info = p.getDebugVisualizerCamera()
+    curr_time = time.time()
+    if curr_time - start_time > 2:
+        found, loc = debugvisualizer.object_is_in_frame(
+            p, panda.panda, prev_loc)
+        if found:
+            prev_loc = loc
+            print("found")
+        else:
+            print("not found")
+        start_time = curr_time
+        x = x + 1
+        cam_info = p.getDebugVisualizerCamera()
+        #p.resetDebugVisualizerCamera(
+        #    cameraDistance=1.4, cameraYaw=40, cameraPitch=-30, cameraTargetPosition=[x, 0, 0])
 
-	panda.step()
-	p.stepSimulation()
-	
-	# prof.profile(127)
-	time.sleep(timeStep)
+    panda.step()
+    p.stepSimulation()
 
-	
+    # prof.profile(127)
+    time.sleep(timeStep)
